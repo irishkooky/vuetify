a87e175ab63989de93215ab80a1e222bacec53d8
irishkooky <yoshihiro.kurosaki.15@gmail.com>
2023-03-24 15:41:00 +0900

    🩹 配列からオブジェクト

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -4,39 +4,37 @@
   // import SideBar from '@/components/DeviceInfoComponent.vue'
   import DeviceInfoComponent from '@/components/DeviceInfoComponent.vue'
 
-  const deviceInfo = [
-    {
-      asNumber: '65534',
-      rd: '65534:100',
-      exportPolicy: 'SET-C10',
-      interface: [
-        {
-          name: 'bundleEther1.100',
-          detail: {
-            description: 'to-Node11',
-            ipAddress: [{ address: '192.168.21.1/24' }],
-          },
+  const deviceInfo = {
+    asNumber: '65534',
+    rd: '65534:100',
+    exportPolicy: 'SET-C10',
+    interface: [
+      {
+        name: 'bundleEther1.100',
+        detail: {
+          description: 'to-Node11',
+          ipAddress: [{ address: '192.168.21.1/24' }],
         },
-        {
-          name: 'gigabitEthernet0/0/0/0',
-          detail: {
-            description: 'to - Node21',
-            ipAddress: [
-              { address: '192.168.21.1/24' },
-              { address: '192.168.22.1/29', secondary: true },
-              { address: '192.168.23.1/29', secondary: true },
-            ],
-          },
-        },
-      ],
-      routing: {
-        bgpNeighbor: [
-          { address: '192.168.21.2', as: '18000' },
-          { address: '192.168.21.3', as: '18000' },
-        ],
       },
+      {
+        name: 'gigabitEthernet0/0/0/0',
+        detail: {
+          description: 'to - Node21',
+          ipAddress: [
+            { address: '192.168.21.1/24' },
+            { address: '192.168.22.1/29', secondary: true },
+            { address: '192.168.23.1/29', secondary: true },
+          ],
+        },
+      },
+    ],
+    routing: {
+      bgpNeighbor: [
+        { address: '192.168.21.2', as: '18000' },
+        { address: '192.168.21.3', as: '18000' },
+      ],
     },
-  ]
+  }
   // const deviceInfo = axios.get('https://cisco/deviceInfo')
 </script>
 
