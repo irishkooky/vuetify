c00b5572ecd309f55e9a7571eb3ba8147ca20ca2
irishkooky <yoshihiro.kurosaki.15@gmail.com>
2023-03-24 15:08:43 +0900

    🩹 データ変更

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -36,38 +36,6 @@
         ],
       },
     },
-    {
-      asNumber: '65534',
-      rd: '65534:100',
-      importPolicy: 'SET-C10',
-      exportPolicy: 'SET-C10',
-      interface: [
-        {
-          name: 'bundleEther1.100',
-          detail: {
-            description: 'to-Node11',
-            ipAddress: [{ address: '192.168.21.1/24' }],
-          },
-        },
-        {
-          name: 'gigabitEthernet0/0/0/0',
-          detail: {
-            description: 'to - Node21',
-            ipAddress: [
-              { address: '192.168.21.1/24' },
-              { address: '192.168.22.1/29', secondary: true },
-              { address: '192.168.23.1/29', secondary: true },
-            ],
-          },
-        },
-      ],
-      routing: {
-        bgpNeighbor: [
-          { address: '192.168.21.2', as: '18000' },
-          { address: '192.168.21.3', as: '18000' },
-        ],
-      },
-    },
   ]
   // const deviceInfo = axios.get('https://cisco/deviceInfo')
 </script>
