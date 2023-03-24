aa22f24dad6dd325e2d304db73912801e63b7328
irishkooky <yoshihiro.kurosaki.15@gmail.com>
2023-03-24 12:41:43 +0900

    🩹 順番入れ替え

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+  // import DefaultBar from './AppBar.vue'
+  import DefaultView from './View.vue'
+  // import SideBar from '@/components/DeviceInfoComponent.vue'
+  import DeviceInfoComponent from '@/components/DeviceInfoComponent.vue'
+</script>
+
 <template>
   <v-app>
     <!-- <default-bar /> -->
@@ -5,10 +12,3 @@
     <default-view />
   </v-app>
 </template>
-
-<script lang="ts" setup>
-  // import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
-  // import SideBar from '@/components/DeviceInfoComponent.vue'
-  import DeviceInfoComponent from '@/components/DeviceInfoComponent.vue';
-</script>
